5638734e434341436f14b69dcf5822476de49b25
SHIAK AFRID <afridayan01@gmail.com>
2025-03-20 16:22:29 +0530

    Update stationApis.ts

--- src/services/stationApis.ts
+++ src/services/stationApis.ts
@@ -1,6 +1,6 @@
 import { stationRoutePayloadType } from "@/types/appDatatypes";
 
-const baseUrl = "https://hyderabadmetroapi.vercel.app";
+const baseUrl = "https://hyderabad-metro-api.onrender.com";
 
 export async function getAllStationsAPI() {
   const server = await fetch(baseUrl + "/api/allstations");
